d1fa172f82370cf7dcf6e9301e41faef7089bc0b
Ramil Latypov <albedo@MacBook-Air-Ramil.local>
2024-07-18 05:14:00 +0500

    Main header file.

---
+++ Hina.hpp
@@ -0,0 +1,5 @@
+#include "./DataLoader/dataloader.cpp"
+#include "./LinearAlgebra/linearalgebra.cpp"
+#include "./Metrics/metrics.cpp"
+#include "./LossGradients/lossgradients.cpp"
+#include "./Optimizers/linear_stochastic_gradient_descent_optimizer.cpp"
