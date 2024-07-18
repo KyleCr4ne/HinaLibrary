d1b281f147844f0b426a936488bf0f9072b03f22
Ramil Latypov <albedo@MacBook-Air-Ramil.local>
2024-07-19 04:33:39 +0500

    Updated main header file

--- Hina.hpp
+++ Hina.hpp
@@ -4,3 +4,4 @@
 #include "./LossGradients/lossgradients.cpp"
 #include "./Optimizers/linear_stochastic_gradient_descent_optimizer.cpp"
 #include "./LinearModels/linear_models.cpp"
+#include "./Tree/decision_tree.hpp"
