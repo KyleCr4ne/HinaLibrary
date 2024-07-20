d10087d702626294caadd11a3b71c7783f3e803e
Ramil Latypov <albedo@MacBook-Air-Ramil.local>
2024-07-20 18:44:17 +0500

    Updated main header file

--- Hina.hpp
+++ Hina.hpp
@@ -5,3 +5,7 @@
 #include "./Optimizers/linear_stochastic_gradient_descent_optimizer.cpp"
 #include "./LinearModels/linear_models.cpp"
 #include "./Tree/decision_tree_classifier.cpp"
+#include "./Tree/decision_tree_regressor.cpp"
+#include "./RandomForest/random_forest.cpp"
+#include "./RandomForest/random_forest_classifier.cpp"
+#include "./RandomForest/random_forest_regressor.cpp"
